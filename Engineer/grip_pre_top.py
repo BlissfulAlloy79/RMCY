c6d7f67423e477ddcf2934b38540838163ba273c
BlissfulAlloy79 <xutony2006@gmail.com>
2023-07-25 10:58:00 +0800

    adjusted parameters

--- Engineer/grip_pre_top.py
+++ Engineer/grip_pre_top.py
@@ -9,7 +9,7 @@ def grip_rest():
     if servo_ctrl.get_angle(1) <= 65 or (80 >= servo_ctrl.get_angle(2) >= 100):
         servo_ctrl.recenter(1, wait_for_complete=True)
         time.sleep(0.5)
-        servo_ctrl.set_angle(2, 90, wait_for_complete=True)
+        servo_ctrl.set_angle(2, 0, wait_for_complete=True)
         servo_ctrl.set_angle(1, 75, wait_for_complete=True)
 
 
